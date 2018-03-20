887fbb7e693e9c825cc367853df13a2c5bce4b7d
Isaac <ihawkins@bluecanopy.com>
2018-03-20 13:37:26 -0400

    set up header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extend Component {
+  render() {
+    return (
+      <nav className="">
+        <ul>
+
+        </ul>
+      </nav>
+    )
+  }
+}
