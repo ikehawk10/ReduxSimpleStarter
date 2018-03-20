7724808c0da84d745c01415befb3d9b9d0562ee2
Isaac <ihawkins@bluecanopy.com>
2018-03-20 16:01:32 -0400

    add Resources component

---
+++ src/components/Resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Favorite JavaScript Stuff
+      <ul>
+        <li>React</li>
+        <li>Ember</li>
+      </ul>
+    </div>
+  )
+}
